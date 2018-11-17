9d94e4e0a891eda97ef32da42b48bb2b3d0bb02c
KTI - Richard Clark <richard@kerkhofftech.ca>
2018-11-17 10:14:14 -0800

    Add notes on room, and set default room for demo

--- chatbot1.done/chatbot1.py
+++ chatbot1.done/chatbot1.py
@@ -4,9 +4,12 @@ from slackclient import SlackClient
 slack_token = os.environ["SLACK_API_TOKEN"]
 sc = SlackClient(slack_token)
 
+# Get the group channels with the following
+# for group in sc.api_call("groups.list").get("groups"):
+#    print("{}-{}".format(group['id'],group['name']))
+
 sc.api_call(
   "chat.postMessage",
-  channel="C0XXXXXX",
+  channel="GDZULG3U0",
   text="Hello from Python! :tada:"
 )
-
